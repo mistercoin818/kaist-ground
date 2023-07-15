39c38b53aac30570005613b0d04f7f697962d4a0
박연진 <2020002960@hanyang.ac.kr>
2023-07-16 03:38:22 +0900

    casing

---
+++ Assets/Scripts/MemoryPool.cs
@@ -0,0 +1,135 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemoryPool : MonoBehaviour
+{
+    // 메모리 풀로 관리되는 오브젝트 정보
+    private class PoolItem {
+        public bool isActive;                   // "gameObject"의 활성화/비활성화 정보
+        public GameObject gameObject;           // 화면에 보이는 실제 게임오브젝트
+    }
+
+    private int increaseCount = 5;              // 오브젝트가 부족할 때 Instanctiate()로 추가 생성되는 오브젝트 개수
+    private int maxCount;                       // 현재 리스트에 등록되어 있는 오브젝트 개수
+    private int activeCount;                    // 현재 게임에 사용되고 있는(활성화) 오브젝트 개수
+    
+    private GameObject poolObject;              // 오브젝트 풀링에서 관리하는 게임 오브젝트 프리팹
+    private List<PoolItem> poolItemList;        // 관리되는 모든 오브젝트를 저장하는 리스트
+
+    public int MaxCount => maxCount;            // 외부에서 현재 리스트에 등록되어 있는 오브젝트 개수 확인을 위한 프로퍼티
+    private int ActiveCount => activeCount;     // 외부에서 현재 활성화 되어 있는 오브젝트 개수 확인을 위한 프로퍼티
+
+    public MemoryPool(GameObject poolObject) {
+        maxCount = 0;
+        activeCount = 0;
+        this.poolObject = poolObject;
+
+        poolItemList = new List<PoolItem>();
+
+        InstantiateObjects();
+    }
+
+    /// increaseCount 단위로 오브젝트를 생성
+    public void InstantiateObjects() {
+        maxCount += increaseCount;
+
+        for (int i = 0; i < increaseCount; ++i) {
+            PoolItem poolItem = new PoolItem();
+
+            poolItem.isActive = false;
+            poolItem.gameObject = GameObject.Instantiate(poolObject);
+            poolItem.gameObject.SetActive(false);
+
+            poolItemList.Add(poolItem);
+        }
+    }
+
+    /// 현재 관리중인(활성/비활성) 모든 오브젝트를 삭제
+    public void DestroyObjects() {
+        if (poolItemList == null) return;
+
+        int count = poolItemList.Count;
+        for (int i = 0; i < count; ++i) {
+            GameObject.Destroy(poolItemList[i].gameObject);
+        }
+
+        poolItemList.Clear();
+    }
+
+    /// poolItemList에 저장되어 있는 오브젝트를 활성화해서 사용
+    /// 현재 모든 오브젝트가 사용중이면 InstantiateObjects()로 추가 생성
+    public GameObject ActivatePoolItem() {
+        if (poolItemList == null) return null;
+
+        // 현재 생성해서 관리하는 모든 오브젝트 개수와 현재 활성화 상태인 오브젝트 개수 비교
+        // 모든 오브젝트가 활성화 상태이면 새로운 오브젝트 필요
+        if (maxCount == activeCount) {
+            InstantiateObjects();
+        }
+
+        int count = poolItemList.Count;
+        for (int i = 0; i < count; ++i) {
+            PoolItem poolItem = poolItemList[i];
+
+            if (poolItem.isActive == false) {
+                ++activeCount;
+
+                poolItem.isActive = true;
+                poolItem.gameObject.SetActive(true);
+
+                return poolItem.gameObject;
+            }
+        }
+
+        return null;
+    }
+
+    // 현재 사용이 완료된 오브젝트를 비활성화 상태로 변경
+    public void DeactivatePoolItem(GameObject removeObject) {
+        if (poolItemList == null || removeObject == null) return;
+
+        int count = poolItemList.Count;
+        for (int i = 0; i < count; ++i) {
+            PoolItem poolItem = poolItemList[i];
+
+            if (poolItem.gameObject == removeObject) {
+                --activeCount;
+
+                poolItem.isActive = false;
+                poolItem.gameObject.SetActive(false);
+
+                return;
+            }
+        }
+    }
+
+    // 게임에 사용 중인 모든 오브젝트를 비활성화 상태로 결정
+    public void DeactivateAllPoolItems() {
+        if (poolItemList == null) return;
+
+        int count = poolItemList.Count;
+        for (int i = 0; i < count; ++i) {
+            PoolItem poolItem = poolItemList[i];
+
+            if (poolItem.gameObject != null && poolItem.isActive == true) {
+                poolItem.isActive = false;
+                poolItem.gameObject.SetActive(false);
+            }
+        }
+
+        activeCount = 0;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/WeaponAssaultRifle.cs
+++ Assets/Scripts/WeaponAssaultRifle.cs
@@ -8,6 +8,10 @@ public class WeaponAssaultRifle : MonoBehaviour
     [SerializeField]
     private GameObject muzzleFlashEffect; // 총구 이펙트(On/Off)
 
+    [Header("Spawn Points")]
+    [SerializeField]
+    private Transform casingSpawnPoint; // 탄피 생성 위치
+
     [Header("Audio Clips")]
     [SerializeField]
     private AudioClip audioClipTakeWeapon; // 무기 장착 사운드
@@ -21,10 +25,12 @@ public class WeaponAssaultRifle : MonoBehaviour
 
     private AudioSource audioSource; // 사운드 재생 컴포넌트
     private PlayerAnimatorController animator; // 애니메이션 재생 제어
+    private CasingMemoryPool casingMemoryPool; // 탄피 생성 후 활성/비활성 관리
 
     private void Awake(){
         audioSource = GetComponent<AudioSource>();
         animator = GetComponentInParent<PlayerAnimatorController>();
+        casingMemoryPool = GetComponent<CasingMemoryPool>();
     }
     // Start is called before the first frame update
     private void OnEnable()
@@ -80,6 +86,8 @@ public class WeaponAssaultRifle : MonoBehaviour
             StartCoroutine("OnMuzzleFlashEffect");
             // 공격 사운드 재생
             PlaySound(audioClipFire);
+            // 탄피 생성
+            casingMemoryPool.SpawnCasing(casingSpawnPoint.position, transform.right);
         }
     }
 
